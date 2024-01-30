238e890da87a648485ccbed852cfe908091e7504
Merci Maier <maiercave@gmail.com>
2024-01-30 09:36:35 -0500

    tables controller

--- back-end/tables/tables.controller.js
+++ back-end/tables/tables.controller.js
@@ -17,3 +17,47 @@ async function create(req, res, next) {
   const data = await tablesService.create(req.body.data);
   res.status(201).json({ data });
 }
+
+// update a table
+async function update(req, res) {
+    const updatedTable = {
+      ...req.body.data,
+      table_id: res.locals.table.table_id,
+    };
+  
+    const updatedReservation = {
+      ...res.locals.reservation,
+      reservation_id: res.locals.reservation.reservation_id,
+      status: "seated",
+    };
+    await reservationsService.updateStatus(updatedReservation);
+  
+    const data = await tablesService.update(updatedTable);
+    res.status(200).json({ data });
+  }
+  
+  // read a table
+  async function read(req, res, next) {
+    res.status(200).json({ data: res.locals.table });
+  }
+  
+  // delete a table
+  async function destroy(req, res, next) {
+    const updatedTable = {
+      ...res.locals.table,
+      reservation_id: null,
+    };
+  
+    const reservation_id = res.locals.table.reservation_id;
+    const reservation = await reservationsService.read(reservation_id);
+  
+    const updatedReservation = {
+      ...reservation,
+      status: "finished",
+    };
+  
+    const data = await tablesService.update(updatedTable);
+    await reservationsService.updateStatus(updatedReservation);
+    res.json({ data });
+  }
+  
