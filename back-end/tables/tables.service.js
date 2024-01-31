ff5f33dce6921f83a1d50d32f3dd25295c58cb4a
Merci Maier <maiercave@gmail.com>
2024-01-30 20:51:54 -0500

    app updated and tables service started

---
+++ back-end/tables/tables.service.js
@@ -0,0 +1,12 @@
+const knex = require("../db/connection");
+
+function list() {
+  return knex("tables").select("*").orderBy("table_name");
+}
+
+function create(table) {
+  return knex("tables")
+    .insert(table)
+    .returning("*")
+    .then((createdTable) => createdTable[0]);
+}
